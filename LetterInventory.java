a39f4b8e330e7a7bb9bc9ea5d8528413191d1078
Jack Albrecht <124408265+Luckyjaq@users.noreply.github.com>
2023-06-06 00:26:32 -0700

    Notes LetterInventory.java

--- LetterInventory.java
+++ LetterInventory.java
@@ -1,9 +1,11 @@
 package letterinventory;
 
 public class LetterInventory {
+   // Represents the size of the alphabet
    private static final int ALPHABET_SIZE = 26;
+   // Array to store the counts of each letter in the inventory
    private int[] inventory;
-
+   // Constructor to initialize the inventory
    public LetterInventory(String data) { 
       inventory = new int[ALPHABET_SIZE];
       data = data.toLowerCase();
@@ -16,7 +18,7 @@ public class LetterInventory {
          }
       }
    }
-
+   // Returns the count of a specific letter in the inventory
    public int get(char letter) {
       letter = Character.toLowerCase(letter);
       if (!Character.isLetter(letter)) {
@@ -26,7 +28,7 @@ public class LetterInventory {
       int index = letter - 'a';
       return inventory[index];
    }
-
+   // Sets the count of a specific letter in the inventory
    public void set(char letter, int value) {
       letter = Character.toLowerCase(letter);
       if (!Character.isLetter(letter) || value < 0) {
@@ -36,7 +38,7 @@ public class LetterInventory {
       int index = letter - 'a';
       inventory[index] = value;
    }
-
+   // Returns the size of the inventory (total count of all letters) 
    public int size() {
       int sum = 0;
       for (int count : inventory) {
@@ -44,7 +46,7 @@ public class LetterInventory {
       }
       return sum;
    }
-
+   // Checks if the inventory is empty (all letter counts are zero)
    public boolean isEmpty() {
       for (int count : inventory) {
          if (count > 0) {
@@ -53,7 +55,7 @@ public class LetterInventory {
       }
       return true;
    }
-
+   // Returns a string representation of the inventory
    public String toString() {
       StringBuilder sb = new StringBuilder();
       sb.append('[');
@@ -66,7 +68,7 @@ public class LetterInventory {
       sb.append(']');
       return sb.toString();
    }
-
+   // Adds the counts of letters from another inventory to this inventory
    public LetterInventory add(LetterInventory other) {
       LetterInventory result = new LetterInventory("");
       for (int i = 0; i < ALPHABET_SIZE; i++) {
@@ -74,7 +76,7 @@ public class LetterInventory {
       }
       return result;
    }
-
+   // Subtracts the counts of letters from another inventory from this inventory
    public LetterInventory subtract(LetterInventory other) {
       LetterInventory result = new LetterInventory("");
       for (int i = 0; i < ALPHABET_SIZE; i++) {
